b34682f57ecd1033a5f090460fc238b62937d94f
chichiadd <113003526+chichiadd@users.noreply.github.com>
2024-10-25 09:26:47 +0800

    Create MajorPlan.java

---
+++ 培训计划管理/MajorPlan.java
@@ -0,0 +1,73 @@
+public class MajorPlan {
+    // 属性
+    private String majorName;          // 专业名称
+    private Integer majorId;              // 专业编号
+    private String trainingPurpose;    // 培训目的
+    private String trainingContent;    // 培训内容
+    private Integer classHours;        // 课时
+    private String teacher;            // 培训教师
+
+    // 构造方法
+    public MajorPlan() {
+    }
+
+    public MajorPlan(String majorName, Integer majorId, String trainingPurpose, String trainingContent, Integer classHours,
+                     String teacher) {
+        this.majorName = majorName;
+        this.majorId = majorId;
+        this.trainingPurpose = trainingPurpose;
+        this.trainingContent = trainingContent;
+        this.classHours = classHours;
+        this.teacher = teacher;
+    }
+
+    // getter和setter方法
+    public String getMajorName() {
+        return majorName;
+    }
+
+    public void setMajorName(String majorName) {
+        this.majorName = majorName;
+    }
+
+    public Integer getMajorId() {
+        return majorId;
+    }
+
+    public void setMajorId(Integer majorId) {
+        this.majorId = majorId;
+    }
+
+    public String getTrainingPurpose() {
+        return trainingPurpose;
+    }
+
+    public void setTrainingPurpose(String trainingPurpose) {
+        this.trainingPurpose = trainingPurpose;
+    }
+
+    public String getTrainingContent() {
+        return trainingContent;
+    }
+
+    public void setTrainingContent(String trainingContent) {
+        this.trainingContent = trainingContent;
+    }
+
+    public Integer getClassHours() {
+        return classHours;
+    }
+
+    public void setClassHours(Integer classHours) {
+        this.classHours = classHours;
+    }
+
+    public String getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(String teacher) {
+        this.teacher = teacher;
+    }
+
+}
